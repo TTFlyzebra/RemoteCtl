6ddf5c79e9ea14a6ade880cc1fc21cce2a00dce9
FlyZebra <flycnzebra@gamil.com>
2020-11-06 13:40:11 +0800

    fixed:restart scrcpyserver

--- remotectl/src/main/java/com/flyzebra/remotectl/connect/LocalSocketClient.java
+++ remotectl/src/main/java/com/flyzebra/remotectl/connect/LocalSocketClient.java
@@ -4,7 +4,6 @@ import android.net.LocalSocket;
 import android.net.LocalSocketAddress;
 
 import com.flyzebra.scrcpy.DesktopConnection;
-import com.flyzebra.utils.ByteUtil;
 import com.flyzebra.utils.FlyLog;
 
 import java.io.IOException;
@@ -85,7 +84,7 @@ public class LocalSocketClient implements ISocketTask {
                             sendByteBuffer.compact();
                         }
                         outputStream.write(sendBuffer, 0, sendLen);
-                        FlyLog.d("recv:%s", ByteUtil.bytes2String(sendBuffer,Math.min(sendLen,40)));
+//                        FlyLog.d("recv:%s", ByteUtil.bytes2String(sendBuffer,Math.min(sendLen,40)));
                     }
                 } catch (Exception e) {
                     FlyLog.e(e.toString());
